68715ff9f407cfc07928eebed2d0977e32acdd3a
SullenGod <66937827+SULLENGOD@users.noreply.github.com>
2022-01-11 17:02:13 -0600

    Add files via upload

---
+++ Semana_02/Inc_06/app.js
@@ -0,0 +1,13 @@
+var nombre = prompt("Ingresar nombre")
+var apellido =  prompt("Ingresar apellido")
+var año = prompt("ingresar año de nacimiento")
+var dia = prompt("ingresar dia de nacimiento")
+var mes = prompt("ingresar mes de nacimiento")
+var cursos = prompt("cursos tomados")
+
+console.log(parseInt(cursos) + 1);
+
+if (año < 2000) {
+    alert("Seguro que eres un programador jr?");
+    
+}
